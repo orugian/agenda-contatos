bcfb04bb4b6abbb73a1005986071d086a57f12d0
Guilherme Orugian <orugian@outlook.com>
2023-11-27 16:24:53 -0300

    commit para revisão

---
+++ agenda.js
@@ -0,0 +1,40 @@
+const form = document.getElementById('form-atividade');
+let linhas = ' ';
+
+form.addEventListener('submit', function(e) {
+
+  e.preventDefault;
+  adicionaLinha();
+  linha();
+})
+
+
+function adicionaLinha(){
+
+  const inputNomeContato = document.getElementById('nome-contato');
+  const inputNumeroContato = document.getElementById('numero-contato');
+
+
+  let linha = ' ';
+    linha += '<tr>';
+    linha += `<td>${inputNomeContato.value}</td>`;
+    linha += `<td>${inputNumeroContato.value}</td>`;
+    linha += '</tr>';
+
+    linhas += linha;
+
+    console.log(linhas);
+
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+
+
+    
+  }
+
+
+
+function linha(){
+  const corpoTabela = document.querySelector('tbody');
+  corpoTabela.innerHTML = linhas;
+}
